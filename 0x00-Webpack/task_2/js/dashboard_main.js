ded4d45feba3f50a314b77ef1b7cc77843922d29
theotsori <theodoretsori@gmail.com>
2023-07-24 17:54:50 +0300

    added images and css file

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,6 +2,7 @@ import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
+$('body').append('<div id="logo"></div>')
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
@@ -11,7 +12,7 @@ $('body').append('<p>Copyright - Holberton School</p>');
 let conunt = 0;
 
 function updateCounter() {
-  count += 1;
+  count++;
   $("#count").html(`${count} clicks on the button`);
 };
 
