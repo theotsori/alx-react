af5a9d56e60ad54928487d9ebd5bb518c94092dc
theotsori <theodoretsori@gmail.com>
2023-07-26 18:33:46 +0300

    added test modules

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('should render a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-header')).toHaveLength(1);
+  });
+
+  it('should render a div with class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-body')).toHaveLength(1);
+  });
+
+  it('should render a div with class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-footer')).toHaveLength(1);
+  });
+});
