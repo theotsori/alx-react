063ac316db9de7b8eb81a33528857551b88218f7
theotsori <theodoretsori@gmail.com>
2023-07-27 10:37:07 +0300

    Webpack config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,24 @@
-import $ from "jquery";
+import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
-$('body').append('<p>Copyright - Holberton School</p>');
-
-let count = 0;
-
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
+  let count = 0;
+  const countElement = $('#count');
+
+  $('#clickButton').on('click', _.debounce(function () {
+    count++;
+    countElement.text(`${count} clicks on the button`);
+  }, 300));
 }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+$(document).ready(function () {
+  const body = $('body');
+
+  body.append('<p>Holberton Dashboard</p>');
+  body.append('<p>Dashboard data for the students</p>');
+  body.append('<button id="clickButton">Click here to get started</button>');
+  body.append('<p id="count"></p>');
+  body.append('<p>Copyright - Holberton School</p>');
+
+  updateCounter();
+});
